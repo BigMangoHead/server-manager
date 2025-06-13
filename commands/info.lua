ee998bf9d7894b3f787ca7970317f127d9d4bc31
Isaac Mango <bigmangohead@gmail.com>
2025-06-13 00:33:51 -0500

    Add whether frozen or not to status

--- commands/info.lua
+++ commands/info.lua
@@ -5,9 +5,17 @@ local jsonhandler = require("jsonhandler")
 local info = {}
 
 function info.status()
+    local freezeFile = io.open(FREEZE_FILE_PATH)
+    local freezeStatus = freezeFile:read("*all")
+    freezeFile:close()
     local serverData, onlineServerData = jsonhandler.getServers()
     local screens = screenhandler.getScreens()
 
+    if freezeStatus == 0 then
+        print("Servers currently not frozen.")
+    else
+        print("SERVERS CURRENTLY FROZEN.")
+    end
     print("Current running screens: [" .. table.concat(screens, ", ") .. "]")
     print("Online servers: ")
     for k, v in pairs(onlineServerData) do
