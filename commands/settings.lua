6066824a1f22c5e447c72823393f7b1a10790e6c
Isaac Mango <bigmangohead@gmail.com>
2026-06-22 22:02:09 -0500

    More helpful messages when changing settings

--- commands/settings.lua
+++ commands/settings.lua
@@ -2,15 +2,17 @@ local jsonhandler = require("jsonhandler")
 
 local settings = {}
 
-local VALID_OPTIONS = {"limit"}
+local OPTIONS = {
+    ["limit"] = "control whether RAM and processor limits are enforced"
+}
 
 function settings.set(args)
     local key = args.key
     local val = args.value
 
     local found = false
-    for _, value in ipairs(VALID_OPTIONS) do
-        if key == value then found = true end
+    for x, _ in ipairs(OPTIONS) do
+        if key == x then found = true end
     end
     if not found then
         parser:error("Key is invalid")
@@ -27,6 +29,12 @@ function settings.set(args)
         miscServerData[key] = true
     end
     jsonhandler.updateMiscServerData(miscServerData)
+    if (val == "false") then
+        print("Enabled setting to " .. OPTIONS[key])
+    else
+        print("Disabled setting to " .. OPTIONS[key])
+    end
+
 
     if not args.no_update then
         require("update").run()
